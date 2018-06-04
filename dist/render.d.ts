a8f5461a89eb3b11e14521a648be8ce80148b9fb
2627500295 <2627500295@qq.com>
2018-06-04 15:04:40 +0800

    v0.3.1

--- dist/render.d.ts
+++ dist/render.d.ts
@@ -1,3 +1,6 @@
-declare const render: Function;
-declare const hydrate: Function;
-export { render as default, render, hydrate };
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+export declare type IRenderReturn = (RCC: React.ComponentClass) => void;
+export declare function render(MountElement?: HTMLElement, Renderer?: ReactDOM.Renderer): IRenderReturn;
+export declare function hydrate(MountElement?: HTMLElement): IRenderReturn;
+export default render;
--- dist/render.js
+++ dist/render.js
@@ -1,27 +1,27 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-// const appElem: HTMLElement = document.querySelector('#app');
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = __importStar(require("react"));
+var ReactDOM = __importStar(require("react-dom"));
 var appElem = document.getElementById('app');
-// NOTE: React v1.0
-// const render = (
-//   Component: React.ComponentClass,
-//   mountNode: HTMLElement | null = appElem
-// ) => {
-//   ReactDOM.render(<Component />, mountNode);
-// };
-// Usage
-//    @render
-// NOTE: Render v2.0
-var render = function (mountNode) {
-    if (mountNode === void 0) { mountNode = appElem; }
-    return function (Component) {
-        ReactDOM.render(React.createElement(Component, null), mountNode);
+var rootElem = document.getElementById('root');
+function render(MountElement, Renderer) {
+    if (MountElement === void 0) { MountElement = appElem; }
+    if (Renderer === void 0) { Renderer = ReactDOM.render; }
+    var MountNode = MountElement || rootElem;
+    return function (RCC) {
+        Renderer(React.createElement(RCC, null), MountNode);
     };
-};
-var hydrate = function (mountNode) {
-    if (mountNode === void 0) { mountNode = appElem; }
-    return function (Component) {
-        ReactDOM.hydrate(React.createElement(Component, null), mountNode);
-    };
-};
-export { render as default, render, hydrate };
+}
+exports.render = render;
+function hydrate(MountElement) {
+    return render(MountElement, ReactDOM.hydrate);
+}
+exports.hydrate = hydrate;
+exports.default = render;
