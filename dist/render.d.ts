325a4fde540fa9c160fc688ae6bc4e3de923d7e1
2627500295 <2627500295@qq.com>
2018-06-07 15:33:04 +0800

    元素逻辑处理

--- dist/render.d.ts
+++ dist/render.d.ts
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-export declare type IRenderReturn = (RCC: React.ComponentClass) => void;
-export declare function render(MountElement?: HTMLElement, Renderer?: ReactDOM.Renderer): IRenderReturn;
-export declare function hydrate(MountElement?: HTMLElement): IRenderReturn;
-export default render;
+import React from 'react';
+import ReactDOM from 'react-dom';
+export declare type IRenderReturn = (ReactComponentClass: React.ComponentClass) => void;
+export declare function render(MountElement?: HTMLElement | null, Renderer?: ReactDOM.Renderer): IRenderReturn;
+export declare function hydrate(MountElement?: HTMLElement | null): IRenderReturn;
+export { render as default };
